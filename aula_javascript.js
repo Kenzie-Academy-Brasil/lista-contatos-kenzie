39e93646b11301707ec4ffd6afcc7b94a00d41f2
Maria Porcina <maria@kenzie.com.br>
2022-02-05 18:11:26 -0300

    primeira parte aula js

---
+++ aula_javascript.js
@@ -0,0 +1,71 @@
+// console.log('hello world');
+
+// Variáveis
+let idade = "21";
+// const nome = "Maria";
+
+// console.log(idade);
+
+idade = "22";
+
+// console.log(idade);
+
+// Tipos de dados
+/*
+string
+- sempre entre aspas
+- tudo que for um texto
+
+number/inteiro/float
+- numéros, seja inteiro ou decimal
+
+boolean/booleano
+- verdadeiro => true
+- falso => false
+*/
+
+const verdadeiroFalso = "false";
+
+// console.log(verdadeiroFalso)
+
+/* tipos de dados => objetos */
+
+// array => listas
+
+// const nome1 = "Maria";
+// const nome2 = "Manu";
+// const nome3 = "Hudson";
+// const nome4 = "Vitor";
+
+// console.log(nome1);
+// console.log(nome2);
+// console.log(nome3);
+// console.log(nome4);
+
+const nomes = ["Maria", 21, "Curitiba", "Manu", 20, "Presidente Prudente", "Hudson", "Vitor", "Yasmin", "Felipe"];
+
+// console.log(nomes);
+// console.log(nomes[9]);
+
+// objeto literal
+
+const maria = {
+    nome: "Maria",
+    idade: 21,
+    cidade: "Curitiba"
+}
+console.log(maria.idade);
+
+const manu = {
+    nome: "Emanuela",
+    idade: 20,
+    cidade: "Presidente Prudente"
+}
+
+// console.log(manu);
+
+const pessoas = [];
+
+// pessoas.push(maria, manu);
+
+// console.log(pessoas);
