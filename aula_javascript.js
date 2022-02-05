1f7677231862635e90d269161905f722c9a89ee4
emanuelakenzie <manu.biq@gmail.com>
2022-02-05 18:58:19 -0300

    feat: aula js

--- aula_javascript.js
+++ aula_javascript.js
@@ -1,12 +1,12 @@
 // console.log('hello world');
 
 // Variáveis
-let idade = "21";
+let idade = '21';
 // const nome = "Maria";
 
 // console.log(idade);
 
-idade = "22";
+idade = '22';
 
 // console.log(idade);
 
@@ -24,7 +24,7 @@ boolean/booleano
 - falso => false
 */
 
-const verdadeiroFalso = "false";
+const verdadeiroFalso = 'false';
 
 // console.log(verdadeiroFalso)
 
@@ -42,7 +42,18 @@ const verdadeiroFalso = "false";
 // console.log(nome3);
 // console.log(nome4);
 
-const nomes = ["Maria", 21, "Curitiba", "Manu", 20, "Presidente Prudente", "Hudson", "Vitor", "Yasmin", "Felipe"];
+const nomes = [
+    'Maria',
+    21,
+    'Curitiba',
+    'Manu',
+    20,
+    'Presidente Prudente',
+    'Hudson',
+    'Vitor',
+    'Yasmin',
+    'Felipe',
+];
 
 // console.log(nomes);
 // console.log(nomes[9]);
@@ -50,17 +61,17 @@ const nomes = ["Maria", 21, "Curitiba", "Manu", 20, "Presidente Prudente", "Huds
 // objeto literal
 
 const maria = {
-    nome: "Maria",
+    nome: 'Maria',
     idade: 21,
-    cidade: "Curitiba"
-}
+    cidade: 'Curitiba',
+};
 console.log(maria.idade);
 
 const manu = {
-    nome: "Emanuela",
+    nome: 'Emanuela',
     idade: 20,
-    cidade: "Presidente Prudente"
-}
+    cidade: 'Presidente Prudente',
+};
 
 // console.log(manu);
 
@@ -69,3 +80,61 @@ const pessoas = [];
 // pessoas.push(maria, manu);
 
 // console.log(pessoas);
+
+const estouComSono = false;
+
+// No if colocamos a condição
+// Cai no else se a condição do if nao der certo
+
+if (estouComSono === false) {
+    console.log('Vou estudar');
+} else {
+    console.log('Vou dormir');
+}
+
+// === (estritamente igual, difere os tipos)
+// == (igual)
+// >=
+// >
+// <
+// && satisfazer mais de uma condição
+// || satisfazer pelo menos uma condição
+
+if (manu.idade >= 18 && manu.cidade === 'Presidente Prudente') {
+    console.log('é maior de idade e é de Prudente');
+} else {
+    console.log('é menor de idade');
+}
+
+if (manu.idade >= 18 || manu.cidade === 'Presidente Prudente') {
+    console.log('Você passou em pelo menos uma das condições!');
+} else {
+    console.log('Não passou em nenhuma condição!');
+}
+
+const euEstouComSono = false;
+
+if (!euEstouComSono) {
+    console.log('Não estou com sono');
+}
+
+if (!(manu.idade < 25)) {
+    console.log('é menor do que 25');
+}
+
+function darOi5Vezes(nome) {
+    for (let indice = 0; indice < 5; indice++) {
+        console.log('Oi' + ' ' + nome);
+    }
+}
+
+darOi5Vezes('Maria');
+
+const botao = document.getElementById('btn');
+console.log(botao);
+
+function clicouNoBotao(evento) {
+    console.log(evento);
+}
+
+botao.addEventListener('click', clicouNoBotao);
